29dd83096bf3645111a379505969d8279d5d9c0b
Alain Ballen <alainballen@gmail.com>
2026-02-13 03:27:01 -0500

    add tailwind back

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/valentines-day-surprise-2026/',
 })
